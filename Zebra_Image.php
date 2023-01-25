83eefef50dc8bc438b4e339541895d809f83e323
stefangabos <contact@stefangabos.ro>
2023-01-25 16:37:20 +0200

    Added support for BMP and fixed some issues with WEBP images

--- Zebra_Image.php
+++ Zebra_Image.php
@@ -26,8 +26,8 @@ ini_set('gd.jpeg_ignore_warning', '1');
  *  Read more {@link https://github.com/stefangabos/Zebra_Image/ here}
  *
  *  @author     Stefan Gabos <contact@stefangabos.ro>
- *  @version    2.8.1 (last revision: December 29, 2022)
- *  @copyright  © 2006 - 2022 Stefan Gabos
+ *  @version    2.8.2 (last revision: January 25, 2023)
+ *  @copyright  © 2006 - 2023 Stefan Gabos
  *  @license    https://www.gnu.org/licenses/lgpl-3.0.txt GNU LESSER GENERAL PUBLIC LICENSE
  *  @package    Zebra_Image
  */
@@ -49,6 +49,17 @@ class Zebra_Image {
      *  @var boolean
      */
     public $auto_handle_exif_orientation;
+
+    /**
+     *  Indicates whether BMP images should be compressed with run-length encoding (RLE), or not.
+     *
+     *  >   Used only if the file at {@link target_path} is a `BMP` image, or it will be ignored otherwise.
+     *
+     *  Default is `TRUE`
+     *
+     *  @var boolean
+     */
+    public $bmp_compressed;
 
     /**
      *  Indicates the file system permissions to be set for newly created images.
@@ -189,7 +200,7 @@ class Zebra_Image {
     /**
      *  Path to an image file to apply the transformations to.
      *
-     *  Supported file types are `GIF`, `PNG`, `JPEG` and `WEBP`.
+     *  Supported file types are `BMP`, `GIF`, `JPEG`, `PNG` and `WEBP`.
      *
      *  >   `WEBP` support is available for PHP version `7.0.0+`.<br><br>
      *      Note that even though `WEBP` support was added to PHP in version `5.5.0`, it only started working with version
@@ -197,6 +208,8 @@ class Zebra_Image {
      *      available only if PHP version is at least `7.0.0`<br><br>
      *      Animated `WEBP` images are not currently supported by GD.
      *      See {@link https://github.com/libgd/libgd/issues/648 here} and {@link https://bugs.php.net/bug.php?id=79809&thanks=4 here}.
+     *
+     *  >   `BMP` support is available for PHP version `7.2.0+`
      *
      *  @var    string
      */
@@ -206,7 +219,7 @@ class Zebra_Image {
      *  Path (including file name) to where to save the transformed image.
      *
      *  >   Can be a different format than the file at {@link source_path}. The format of the transformed image will be
-     *      determined by the file's extension. Supported file types are `GIF`, `PNG`, `JPEG` and `WEBP`
+     *      determined by the file's extension. Supported file types are `BMP`, `GIF`, `JPEG`, `PNG` and `WEBP`.
      *
      *  >   `WEBP` support is available for PHP version `7.0.0+`.<br><br>
      *      Note that even though `WEBP` support was added to PHP in version `5.5.0`, it only started working with version
@@ -214,6 +227,8 @@ class Zebra_Image {
      *      available only if PHP version is at least `7.0.0`<br><br>
      *      Animated `WEBP` images are not currently supported by GD.
      *      See {@link https://github.com/libgd/libgd/issues/648 here} and {@link https://bugs.php.net/bug.php?id=79809&thanks=4 here}.
+     *
+     *  >   `BMP` support is available for PHP version `7.2.0+`
      *
      *  @var    string
      */
@@ -295,7 +310,7 @@ class Zebra_Image {
 
         $this->webp_quality = 80;
 
-        $this->preserve_aspect_ratio = $this->preserve_time = $this->enlarge_smaller_images = true;
+        $this->preserve_aspect_ratio = $this->preserve_time = $this->enlarge_smaller_images = $this->bmp_compressed = true;
 
         $this->sharpen_images = $this->auto_handle_exif_orientation = $this->jpeg_interlace = false;
 
@@ -1437,9 +1452,14 @@ class Zebra_Image {
         // and if it founds an unsupported file type
         } elseif (
 
+            ($this->source_type = strtolower(substr($this->source_path, strrpos($this->source_path, '.') + 1))) &&
+
+            !(version_compare(PHP_VERSION, '7.0.0') < 0 && $this->source_type === 'webp') &&
+            !(version_compare(PHP_VERSION, '7.2.0') < 0 && $this->source_type === 'bmp') &&
+
             // getimagesize() doesn't support WEBP until 7.1.0 so we will handle that differently
-            !(version_compare(PHP_VERSION, '7.0.0') >= 0 && version_compare(PHP_VERSION, '7.1.0') < 0 && ($this->source_type = strtolower(substr($this->source_path, strrpos($this->source_path, '.') + 1))) === 'webp') &&
-            !list($this->source_width, $this->source_height, $this->source_type) = @getimagesize($this->source_path)
+            ($this->source_path !== 'webp' && !list($this->source_width, $this->source_height, $this->source_type) = @getimagesize($this->source_path))
+
         ) {
 
             // save the error level and stop the execution of the script
@@ -1462,7 +1482,7 @@ class Zebra_Image {
                 }
 
                 // if PHP version is less than 7.1.0
-                if (version_compare(PHP_VERSION, '7.0.0') >= 0 && version_compare(PHP_VERSION, '7.1.0') < 0) {
+                if (version_compare(PHP_VERSION, '7.1.0') < 0) {
 
                     // flag these so we compute them later on
                     $this->source_width = -1;
@@ -1478,6 +1498,14 @@ class Zebra_Image {
             // create an image from file using extension dependant function
             // checks for file extension
             switch ($this->source_type) {
+
+                // if BMP
+                case IMAGETYPE_BMP:
+
+                    // create an image from file
+                    $identifier = imagecreatefrombmp($this->source_path);
+
+                    break;
 
                 // if GIF
                 case IMAGETYPE_GIF:
@@ -1510,14 +1538,6 @@ class Zebra_Image {
 
                     break;
 
-                // if JPEG
-                case IMAGETYPE_JPEG:
-
-                    // create an image from file
-                    $identifier = imagecreatefromjpeg($this->source_path);
-
-                    break;
-
                 // if PNG
                 case IMAGETYPE_PNG:
 
@@ -1529,6 +1549,14 @@ class Zebra_Image {
 
                     // save full alpha channel information
                     imagesavealpha($identifier, true);
+
+                    break;
+
+                // if JPEG
+                case IMAGETYPE_JPEG:
+
+                    // create an image from file
+                    $identifier = imagecreatefromjpeg($this->source_path);
 
                     break;
 
@@ -1549,7 +1577,7 @@ class Zebra_Image {
                     if (fread($fh, 4) === 'VP8X') {
 
                         // look for the "Animation (A)" bit
-                        fseek($fh, 16);
+                        fseek($fh, 20);
 
                         // is this is an animated WEBP?
                         $is_animated = ((ord(fread($fh, 1)) >> 1) & 1);
@@ -1592,8 +1620,8 @@ class Zebra_Image {
                 default:
 
                     // if unsupported file type
-                    // note that we call this if the file is not GIF, JPG, PNG or WEBP even though the getimagesize function
-                    // handles more image types
+                    // note that we call this if the file is not BMP, GIF, JPG, PNG or WEBP even though the getimagesize function
+                    // might handle more image types
                     $this->error = 4;
 
                     return false;
@@ -1835,7 +1863,7 @@ class Zebra_Image {
             // allocate a transparent color
             $background_color = imagecolorallocatealpha($identifier, 0, 0, 0, 127);
 
-            // we also need to set this for saving gifs
+            // we also need to set this for saving GIFs
             imagecolortransparent($identifier, $background_color);
 
             // save full alpha channel information
@@ -1926,6 +1954,29 @@ class Zebra_Image {
 
         // image saving process goes according to required extension
         switch ($this->target_type) {
+
+            // if BMP
+            case 'bmp':
+
+                // if GD support for this file type is not available
+                if (!function_exists('imagebmp')) {
+
+                    // save the error level and stop the execution of the script
+                    $this->error = 6;
+
+                    return false;
+
+                // if, for some reason, file could not be created
+                } elseif (@!imagebmp($identifier, $this->target_path, $this->bmp_compressed)) {
+
+                    // save the error level and stop the execution of the script
+                    $this->error = 3;
+
+                    return false;
+
+                }
+
+                break;
 
             // if GIF
             case 'gif':
